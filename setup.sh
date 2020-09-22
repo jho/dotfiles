0612d888211853ec76df21e0bcb6159a594974ae
Joshua Hollander <jholla14@gmail.com>
2020-09-22 15:31:35 -0600

    Adding tmux powerline theme and some other things

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+#TODO: put any other missing software installation steps here
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#echo "source /usr/local/opt/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
+#brew install powerlevel9k
+#brew install tmux?
+
+#https://github.com/erikw/tmux-powerline
+
 rm -rf ~/.bashrc
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 
 rm -rf ~/.bash_profile
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+
 
 rm -rf ~/.tmux.conf
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
