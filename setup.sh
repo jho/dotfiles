96a61dbb879ae97b28c67f1e8f842dc007f7e1c2
Joshua Hollander <jholla14@gmail.com>
2021-08-08 22:21:57 -0600

    Updates

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-git clone https://github.com/erikw/tmux-powerline.git ~/.tmux
+#git clone https://github.com/erikw/tmux-powerline.git ~/.tmux
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
